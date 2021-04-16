0e8b3cf636e67eb58aff302fb0e4edbb9bf31bd5
Garlyks <jesus.rodriguez.in04@gmail.com>
2021-04-16 13:35:55 +0200

    scenario working

--- Assets/Scripts/WinController.cs
+++ Assets/Scripts/WinController.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinController : MonoBehaviour
 {
+
+    public string textValue;
+    public Text textElement;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        textElement.text = "You Win!!";
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        textElement.text = "";
     }
 }
